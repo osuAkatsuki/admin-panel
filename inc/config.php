68270657afa3b1bf403dbd41d883ebed59149707
cmyui <cmyuiosu@gmail.com>
2024-05-26 23:08:40 -0400

    le fix?

--- inc/config.php
+++ inc/config.php
@@ -5,33 +5,29 @@ require_once $df.'/../vendor/vlucas/phpdotenv/src/Dotenv.php';
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
 
-//print db host
-echo 'before ' . getenv('DATABASE_HOST');
-
 // Database config
-define('DATABASE_HOST', getenv('DATABASE_HOST'));	// MySQL host. usually localhost
-echo 'after ' . DATABASE_HOST;
-define('DATABASE_USER', getenv('DATABASE_USER'));		// MySQL username
-define('DATABASE_PASS', getenv('DATABASE_PASS'));		// MySQL password
-define('DATABASE_NAME', getenv('DATABASE_NAME'));		// Database name
-define('DATABASE_WHAT', getenv('DATABASE_WHAT'));		// "host" or unix socket path
+define('DATABASE_HOST', $_ENV['DATABASE_HOST']);	// MySQL host. usually localhost
+define('DATABASE_USER', $_ENV['DATABASE_USER']);		// MySQL username
+define('DATABASE_PASS', $_ENV['DATABASE_PASS']);		// MySQL password
+define('DATABASE_NAME', $_ENV['DATABASE_NAME']);		// Database name
+define('DATABASE_WHAT', $_ENV['DATABASE_WHAT']);		// "host" or unix socket path
 
-define('DISCORD_WEBHOOK_URL', getenv("DISCORD_WEBHOOK_URL"));
+define('DISCORD_WEBHOOK_URL', $_ENV["DISCORD_WEBHOOK_URL"]);
 
 // Server urls, no slash
 $URL = [
-	'avatar' => getenv('AVATAR_BASE_URL'), // 'https://a.akatsuki.pw'
-	'server' => getenv('FRONTEND_BASE_URL'), // 'https://akatsuki.pw',
-	'bancho' => getenv('BANCHO_BASE_URL'), // 'http://c.akatsuki.pw',
+	'avatar' => $_ENV['AVATAR_BASE_URL'], // 'https://a.akatsuki.pw'
+	'server' => $_ENV['FRONTEND_BASE_URL'], // 'https://akatsuki.pw',
+	'bancho' => $_ENV['BANCHO_BASE_URL'], // 'http://c.akatsuki.pw',
 ];
 
 // S3 config
 $S3Config = [
-	'access_key' => getenv('AWS_ACCESS_KEY_ID'),
-	'secret_key' => getenv('AWS_SECRET_ACCESS_KEY'),
-	'region' => getenv('AWS_DEFAULT_REGION'),
-	'bucket' => getenv('AWS_BUCKET_NAME'),
-	'endpoint_url' => getenv('AWS_ENDPOINT_URL'),
+	'access_key' => $_ENV['AWS_ACCESS_KEY_ID'],
+	'secret_key' => $_ENV['AWS_SECRET_ACCESS_KEY'],
+	'region' => $_ENV['AWS_DEFAULT_REGION'],
+	'bucket' => $_ENV['AWS_BUCKET_NAME'],
+	'endpoint_url' => $_ENV['AWS_ENDPOINT_URL'],
 ];
 
 // Scores/PP config
