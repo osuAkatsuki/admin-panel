792b0886ba08bbf8a4fe970a4ded5cf618d275e0
cmyui <cmyuiosu@gmail.com>
2024-06-24 06:00:01 -0400

    Remove unused functions

--- inc/helpers/URL.php
+++ inc/helpers/URL.php
@@ -6,16 +6,4 @@ class URL {
 
 		return isset($URL['avatar']) ? $URL['avatar'] : 'https://a.akatsuki.pw';
 	}
-
-	public static function Server() {
-		global $URL;
-
-		return isset($URL['server']) ? $URL['server'] : 'https://akatsuki.pw';
-	}
-
-	public static function Bancho() {
-		global $URL;
-
-		return isset($URL['bancho']) ? $URL['bancho'] : 'http://c.akatsuki.pw';
-	}
 }
