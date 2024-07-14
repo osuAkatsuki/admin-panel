da4ec7c4b80b60e7d9966521ec0b32127dcd65f9
Mahmood <124461948+ConditionedBeef@users.noreply.github.com>
2024-07-14 01:04:02 +0100

    manage (#41)

--- inc/PrivilegesEnum.php
+++ inc/PrivilegesEnum.php
@@ -25,5 +25,6 @@ class Privileges {
 	const UserTournamentStaff		= 1 << 21;
 	const AdminCaker				= 1 << 22;
 	const UserPremium				= 1 << 23;
-	const AdminFreezeUsers = 1 << 24;
+	const AdminFreezeUsers			= 1 << 24;
+	const AdminManageNominators		= 1 << 25;
 }
