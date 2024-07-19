be565a5d4d19d1e2a56aa15e25eb492abbc39170
Josh Smith <cmyuiosu@gmail.com>
2024-07-19 04:22:36 -0400

    undo change w/ risk

--- index.php
+++ index.php
@@ -100,12 +100,9 @@ if (isset($_GET['p'])) {
 	<link rel="stylesheet" href="https://unpkg.com/alwan/dist/css/alwan.min.css" />
 
     <!-- Favicon -->
-    <link rel="logo" sizes="128x128" href="/logo.png">
-    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
-    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
-    <link rel="manifest" href="/manifest.json">
-    <link rel="mask-icon" href="/safari-pinned-tab.svg?" color="#5bbad5">
-    <link rel="shortcut icon" href="/favicon.ico">
+    <link rel="manifest" href="/manifest.json?v=xQQWRwyGed">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg?v=xQQWRwyGed" color="#5bbad5">
+    <link rel="shortcut icon" href="/favicon.ico?v=xQQWRwyGed">
     <meta name="theme-color" content="#ffffff">
 
     <meta name=viewport content="width=device-width, initial-scale=1">
